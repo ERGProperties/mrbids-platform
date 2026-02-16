6d7ca6bc64ffaa478ea21979abe8e713b3b667b8
Gary Walker <gwalker1121@comcast.net>
2026-02-16 13:38:23 -0600

    Add closed auction to registry for past auctions

---
+++ lib/auctions/2210-mckenzie-ave-waco.ts
@@ -0,0 +1,29 @@
+export const auction2210McKenzie = {
+  slug: "2210-mckenzie-ave-waco",
+  title: "2210 McKenzie Ave",
+  addressLine: "2210 McKenzie Ave",
+  cityStateZip: "Waco, TX",
+
+  // ⛔ PAST DATE = closed auction
+  auctionEnd: "2026-02-15T17:00:00-06:00",
+
+  startingBid: 100000,
+  bidIncrement: 5000,
+  arv: 230000,
+
+  imagesPath: "/auctions/2210-mckenzie-ave-waco",
+  images: ["01-curbside.jpg"],
+
+  description: [
+    "2210 McKenzie Ave was offered as a seller-direct auction on MrBids.",
+    "The auction has closed and is presented as a completed case study.",
+  ],
+
+  propertyFacts: [
+    "Property Type: Single-Family",
+    "Bedrooms: 3",
+    "Bathrooms: 2",
+  ],
+
+  auctionTerms: ["Auction closed"],
+};
--- lib/auctions/index.ts
+++ lib/auctions/index.ts
@@ -1,3 +1,8 @@
 import { auction1604Parkdale } from "./1604-parkdale-dr-wichita-falls";
+import { auction2210McKenzie } from "./2210-mckenzie-ave-waco";
 
-export const allAuctions = [auction1604Parkdale];
+export const allAuctions = [
+  auction1604Parkdale,   // live
+  auction2210McKenzie,   // past
+];
+
