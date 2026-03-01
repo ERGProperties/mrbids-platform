c1d4be917018bdc720cf9d4245bddf1528349e64
Gary Walker <gwalker1121@comcast.net>
2026-03-01 12:55:46 -0600

    Add modern mobile web app meta tag

--- app/layout.tsx
+++ app/layout.tsx
@@ -34,6 +34,9 @@ export default function RootLayout({
         <link rel="manifest" href="/manifest.json" />
         <meta name="theme-color" content="#000000" />
 
+        {/* ⭐ Modern PWA support */}
+        <meta name="mobile-web-app-capable" content="yes" />
+
         {/* ⭐ Apple iOS PWA support */}
         <meta name="apple-mobile-web-app-capable" content="yes" />
         <meta
