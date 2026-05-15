9dab3aa376b2b5e81328a55bd7dcc0f7b95c2a98
Gary Walker <gwalker1121@comcast.net>
2026-05-15 17:05:37 -0400

    Add marketplace bidding backend infrastructure

--- app/api/marketplace-auctions/[id]/bid/route.ts
+++ app/api/marketplace-auctions/[id]/bid/route.ts
@@ -1,7 +1,7 @@
 import { NextResponse } from "next/server"
 import { getServerSession } from "next-auth"
 
-import { authOptions } from "@/authOptions"
+import { authOptions } from "@/lib/authOptions"
 import { prisma } from "@/lib/prisma"
 
 export async function POST(
