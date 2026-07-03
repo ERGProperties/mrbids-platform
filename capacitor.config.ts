0079c273d2c856520c4d8a99d415ee8f83e0256e
Gary Walker <gwalker1121@comcast.net>
2026-07-03 04:20:11 -0400

    Restore Capacitor production server config

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,17 +1,20 @@
 import type { CapacitorConfig } from "@capacitor/cli";
 
 const config: CapacitorConfig = {
-
   appId: "com.mrbids.app",
-
   appName: "MrBids",
 
-  webDir: ".next",
+  webDir: "out",
+
+  server: {
+    url: "https://mrbids.com",
+    cleartext: false,
+    androidScheme: "https",
+  },
 
   ios: {
     scheme: "mrbids",
   },
-
 };
 
 export default config;
